051a0fe4db2f9bf734f6e6a3a5d04414f6e7bb7c
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2018-04-26 14:51:34 +0200

    made between function and documented it.

---
+++ dev/between.rb
@@ -0,0 +1,21 @@
+# Public: sees if the first input number is between the second and third input.
+#
+# num1 - the number being compared.
+#
+# num2 - other number comparing the first one.
+#
+# num3 - other number comparing the first one.
+#
+# Examples
+#
+#   is_even(3, 2, 5)
+#   # => true
+#
+# Returns true.
+def between(num1, num2, num3)
+    if num2 < num1 && num1 < num3 || num1 < num2 && num3 < num1
+        return true
+    else
+        return false
+    end
+end
