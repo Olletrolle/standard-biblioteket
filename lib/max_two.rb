827d317c664834a97b81ccc85edb5b2527573e8a
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2018-05-03 14:02:06 +0200

    Klarade av och dokumenterade max_two
    ändrade en pil

---
+++ lib/max_two.rb
@@ -0,0 +1,18 @@
+# Public: Outputs the largest number of two.
+#
+# num(1 2) - the input intregers being compared.
+#
+# Examples
+#
+#   max_two(34, 87)
+#   # => 87
+#
+# Returns number.
+def max_two(num1, num2)
+
+    if num1 > num2
+        return num1
+    else
+        return num2
+    end
+end
