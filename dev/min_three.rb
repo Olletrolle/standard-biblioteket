908b04c961463af98984e33f3473cb39708577a4
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2018-05-03 13:45:08 +0200

    gjorde min_three onch dokumenterade

---
+++ dev/min_three.rb
@@ -0,0 +1,24 @@
+# Public: Compares and outputs the smallest number of three.
+#
+# num(1 2 3): the different numbers being compared.
+#
+# Examples
+#
+#   (34, 3, 56)
+#   # => 3
+#
+# Returns number.
+def min_three(num1, num2, num3)
+
+    if num1 < num2
+        output = num1
+    else
+        output = num2
+    end
+
+    if output < num3
+        return output
+    else
+        return num3
+    end
+end
