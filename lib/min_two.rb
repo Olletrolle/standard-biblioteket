3e83ea9b73e9e3e2d06e46bb51673fffa025a865
Olletrolle <38528665+Olletrolle@users.noreply.github.com>
2018-05-03 13:28:47 +0200

    Gjorde min_two och dokumenterade klart

---
+++ lib/min_two.rb
@@ -0,0 +1,18 @@
+# Public: Outputs the smallest number of two.
+#
+# number - the input intreger.
+#
+# Examples
+#
+#   min_two(34, 87)
+#   # => 87
+#
+# Returns number.
+def min_two(num1, num2)
+
+    if num1 < num2
+        return num1
+    else
+        return num2
+    end
+end
